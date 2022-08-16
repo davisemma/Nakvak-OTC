0e5692da3011c3ce8bacb53f10cb9f86a7048ae1
davisemma <57413290+davisemma@users.noreply.github.com>
2022-08-16 12:42:38 -0400

    Data cleaning and updating code

--- Script/Data_Cleaning.R
+++ Script/Data_Cleaning.R
@@ -1,5 +1,5 @@
 #Some data cleaning
-data <- read.csv("Point Frame/plot_data.csv") 
+data <- read.csv("Point Frame/plot_data_QC_ELD.csv") 
 test <- select(data, c('plot', 'year', 'y', 'x'))
 
 #Certain (~13) plots have <100 points of observation - 
@@ -20,5 +20,16 @@ x <- hit %>%
 x2 <- filter(x, obs == 1)
 x3 <- merge(data, x2, all.y = TRUE)
 
+#checking that height at hit 1 is indeed the max height listed for the pin
+height <-  select(data, c('plot', 'year', 'y', 'x', 'hit_order', 'height'))
+height_check <- height %>%
+  group_by(plot, year, y, x) %>%
+  mutate(max_height = max(height)) %>%
+  ungroup() %>%
+  filter(height == max_height)
+
+z <- height_check[duplicated(height_check[1:4]) | duplicated(height_check[1:4], fromLast=TRUE),] %>%
+  filter(., year != 2008) %>%
+  mutate(diff = max_height - height)
 
 
---
+++ Script/OTC_Abundance_Plots.R
@@ -0,0 +1,105 @@
+plot_theme <-   theme_few() + 
+  theme(legend.position = "top",
+        legend.justification = c(0,-1),
+        legend.box.margin = margin(t = -5, b = -15, l = -6, unit = "pt"),
+        legend.key.size = unit(1, 'lines'),
+        legend.text = element_text(size = 8),
+        legend.title = element_text(size = 8,),
+        plot.title = element_text(size = 8, vjust = 2, margin = margin(t = 3, unit = 'pt')),
+        plot.title.position = "panel",
+        strip.text.x = element_text(size = 8, face = 'bold', hjust = 0, margin = margin(t = 4, b = 4, l = 0, unit = 'pt')),
+        axis.title.x = element_text(size = 8),
+        axis.text.x = element_text(size = 8),
+        axis.title.y = element_text(size = 8),
+        axis.text.y = element_text(size = 8),
+        axis.ticks = element_line(size = 0.3, ),
+        axis.ticks.length = unit(1.5, "pt"),
+        axis.line = element_line(colour = "#403F3F", size = 0.3),
+        panel.border  = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),)
+
+dry <- sjPlot::plot_models(dry_gram_nb_mod,
+                           dry_forb_p_mod,
+                           dry_sdeci_p_mod,
+                    dry_sever_nb_mod,
+                    dry_lichen_nb_mod,
+                    dry_moss_nb_mod,
+                    title="Dry plots",
+                    axis.labels=c("Treatment [OTC]\n* Year", "Year", "Treatment\n[OTC]"),
+                    p.shape = TRUE,
+                    p.threshold = c(0.05),
+                    show.values=FALSE, show.p=TRUE,
+                    show.legend = FALSE,
+                    vline.color = '#F0F1F3',
+                    value.size = 3,
+                    dot.size = 2,
+                    line.size = 0.5,
+                    spacing = 0.65,
+                    axis.lim = c(0.1, 10))+
+  scale_color_manual(values = c('#85D1F0', '#B765A5', '#A4A4A6', '#5D8A55', '#EF9FEF', '#ECD982'))+
+  plot_theme+
+  theme(plot.margin = margin(3, 6, 3, 6))+
+  scale_shape_manual(values = c(1, 19))
+
+wet <- sjPlot::plot_models(wet_gram_nb_mod,
+                           wet_forb_zanb_mod,       
+                    wet_sdeci_nb_mod,
+                    wet_sever_nb_mod,
+                    wet_lichen_p_mod,
+                    wet_moss_nb_mod,
+                    title="Wet plots",
+                    axis.labels=c("", "", ""),
+                    p.shape = TRUE,
+                    p.threshold = c(0.05),
+                    show.values=FALSE, show.p=TRUE,
+                    show.legend = FALSE,
+                    vline.color = '#F0F1F3',
+                    value.size = 3,
+                    dot.size = 2,
+                    line.size = 0.5,
+                    spacing = 0.65,
+                    axis.lim = c(0.1, 10))+
+  scale_color_manual(values = c('#85D1F0', '#B765A5', '#A4A4A6', '#5D8A55', '#EF9FEF', '#ECD982'))+
+  plot_theme+
+  theme(plot.margin = margin(3, 6, 3, 6))+
+  scale_shape_manual(values = c(1, 19))
+  
+
+fake <- sjPlot::plot_models(dry_gram_nb_mod,
+                           dry_forb_p_mod,
+                           dry_sdeci_p_mod,
+                           dry_sever_nb_mod,
+                           dry_lichen_nb_mod,
+                           dry_moss_nb_mod,
+                           title="Dry plots",
+                           axis.labels=c("Treatment [OTC]\n* Year", "Year", "Treatment\n[OTC]"),
+                           vline.color = 'light grey',
+                           value.size = 3,
+                           dot.size = 2,
+                           line.size = 0.5,
+                           spacing = 0.65,
+                           axis.lim = c(0.1, 10),
+                           legend.title = 'Lifeform',
+                           m.labels = c('Graminoid', 'Forb', 'Deciduous shrub', 'Evergreen shrub', 'Lichen', 'Moss'))+
+  scale_color_manual(values = c('#85D1F0', '#B765A5', '#A4A4A6', '#5D8A55', '#EF9FEF', '#ECD982'))+
+  plot_theme+
+  theme(plot.margin = margin(6, 0, 6, 0))+
+  scale_shape_manual(values = c(1, 19))
+
+legend <- get_legend(fake + theme(legend.position = 'top',
+                                                  legend.box.margin = margin(30, 0, 0, 0),
+                                                  legend.text = element_text(size = 8),
+                                                  legend.title = element_text(size = 8),
+                                                  legend.justification = "top"))
+
+cowplot::plot_grid(dry, wet, legend, ncol = 3, rel_widths = c(1.4, 1, 0.6))
+data <- cowplot::plot_grid(dry, wet, ncol = 2, rel_widths = c(1.35, 1), )
+
+legend <- get_legend(fake + theme(legend.position = 'top',
+                                  legend.box.margin = margin(0, 0, 0, 0),
+                                  legend.text = element_text(size = 8),
+                                  legend.title = element_text(size = 8),
+                                  legend.justification = "left"))
+cowplot::plot_grid(legend, data, ncol = 1, rel_heights = c(0.1, 1))
+  
